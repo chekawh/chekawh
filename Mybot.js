bec4790910ea6e7cefbda5aa1bffddbee949a5fb
chekawh <45906536+chekawh@users.noreply.github.com>
2018-12-31 19:16:29 +0000

    Add files via upload

--- Mybot.js
+++ Mybot.js
@@ -131,7 +131,6 @@ message.channel.send(`Se ha eliminado: ${cantidad} mensajes. Ahora se está más
     .addField("Limpiar mensajes", ".limpiar [cantidad]")
     .addField("Personas con el mismo rol", ".rol [nombredelrol]")
     .addField("Info del Servidor", ".servidor")
-    .setDescription("Opciones de bot de Música.")
     .addField("Usar TeloDoyTodo", ";;play [nombre/cancion] | Luego ;;[numero de la pista]")
     .addField("Usar TeloDoyTodo #2", "!play [Cantante + Cancion]")
     .addField("Usar TeloDoyTodo #3", ".music play [nombre/cancion] | Luego [numero]");
